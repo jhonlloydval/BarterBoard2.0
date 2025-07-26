ba316a7b0fc81a3382d961b08e9dfd8170c59350
Jhon Lloyd Valencia <jhonlloydval@gmail.com>
2025-07-26 18:09:01 +0800

    hmm

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        try (Scanner scanner = new Scanner(System.in)){
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+        }
+    }
+}
